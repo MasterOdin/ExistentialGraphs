b96efb8fde56c4b8e7cbdd6e721b8f9dc9b3ac11
Maxfield Herman <leawoodmax@yahoo.com>
2019-07-16 15:19:36 -0400

    Created mockup ui

--- Initial_Mockup/function.js
+++ Initial_Mockup/function.js
@@ -7,4 +7,19 @@ function changeMode() {
     }
 
     buttons = document.querySelectorAll(".sidebar_button");
+    for(var i = 0; i < buttons.length; i++) {
+        if(head.innerText == "Word Mode") {
+            if(buttons[i].classList.contains("proof_button")) {
+                buttons[i].style.background = "gray";
+            } else {
+                buttons[i].style.background = "white";
+            }
+        } else {
+            if(buttons[i].classList.contains("word_button")) {
+                buttons[i].style.background = "gray";
+            } else {
+                buttons[i].style.background = "white";
+            }
+        }
+    }
 }
