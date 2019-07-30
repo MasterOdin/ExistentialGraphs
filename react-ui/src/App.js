315605c133923932a6aa3aa5a66c7d4625e74943
Palmer Stolly <stollp@rpi.edu>
2019-07-30 15:16:20 -0400

    Redid React-ui npm project, added basic side-menu functionality

---
+++ react-ui/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      //Header?
+      <Sidebar />
+      //Box Sized For Main Drawing (with tabs?) //Box Sized For Tree
+    </div>
+  );
+}
+
+export default App;
