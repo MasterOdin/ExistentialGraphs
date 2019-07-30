96e6a52aacf4f110fce0fbd317a44c5c472c2e3c
Maxfield Herman <leawoodmax@yahoo.com>
2019-07-30 01:24:58 -0400

    Further added functionality, i.e.: placing cuts, creating children elements

--- Initial_Mockup/function2.js
+++ Initial_Mockup/function2.js
@@ -3,14 +3,6 @@ $(document).ready(function(e) {
     var graph = new joint.dia.Graph;
     var paper = new joint.dia.Paper({ el: $('#workHolder'), width: '88%', height: '70%', gridSize: 1, model: graph });
 
-    var r1 = new joint.shapes.basic.Rect({
-        position: { x: 20, y: 20 },
-        size: { width: 200, height: 200 },
-        attrs: {rect: {fill: 'white', stroke: 'black'}}
-    });
-
-    graph.addCells([r1]);
-
     var last_mousex = last_mousey = 0;
     var mousex = mousey = 0;
     var currOperation = "";
@@ -42,10 +34,13 @@ $(document).ready(function(e) {
         if (cellViewsBelow.length) {
             // Note that the findViewsFromPoint() returns the view for the `cell` itself.
             var cellViewBelow = _.find(cellViewsBelow, function(c) { return c.model.id !== cell.id });
-        
             // Prevent recursive embedding.
-            if (cellViewBelow && cellViewBelow.model.get('parent') !== cell.id) {
+            if (cellViewBelow && cellViewBelow.model.get('parent') !== cell.id ) {
+                //&& cellViewBelow.id == 'cut'
+                console.log(cellViewBelow.model);
                 cellViewBelow.model.embed(cell);
+                
+                cellViewBelow.model.fitEmbeds({deep: true, padding: 20});
             }
         }
     });
@@ -58,16 +53,17 @@ $(document).ready(function(e) {
         mouseup: function(e) {
             if ((last_mousex + 10 >= e.pageX - 185 && last_mousex - 10 <= e.pageX - 185) && (last_mousey + 10 >= e.pageY - 55&& last_mousey - 10 <= e.pageY - 55)) {
                 if(currOperation == "Prop") {
-                    var message = prompt("");
+                    var message = prompt("Enter proposition value: ");
                     if(message != null) {
                         var rect = new joint.shapes.standard.Rectangle();
                         rect.position(last_mousex, last_mousey);
                         rect.resize(10 + 10 * message.length, 20);
                         rect.attr({
+                            id: 'pro',
                             body: {
                                 fill: 'white',
-                                stroke: 'black',
-                                fillOpacity: '0.0'
+                                fillOpacity: '0.0',
+                                strokeOpacity: '0.0'
                             },
                             label: {
                                 text: message,
@@ -76,19 +72,35 @@ $(document).ready(function(e) {
                         });
                         rect.addTo(graph);
                     }
+                } else if(currOperation == "Cut") {
+                    console.log("epic gamer moment");
+                    var cut = new joint.shapes.basic.Rect({
+                        id: 'cut',
+                        position: { x: last_mousex, y: last_mousey },
+                        size: { width: 200, height: 200 },
+                        attrs: {rect: {fill: 'white', stroke: 'black'}}
+                    });
+                    cut.addTo(graph);
                 }
             }
         }
     });
 
     $(".sidebar_button").on('click', function(e) {
-        console.log(currOperation);
-        if(currOperation != this.innerHTML){
-            currOperation = this.innerHTML;
-            $(this).css("background-color", "rgb(211, 138, 138)");
-        } else {
+        if(currOperation == this.innerHTML) {
             currOperation = "";
             $(this).css("background-color", "white");
+        } else {
+            for(var i = 0; i < $(".sidebar_button").length; i++) {
+                if($(".sidebar_button")[i].innerHTML == currOperation) {
+                    console.log($(".sidebar_button")[i].innerHTML);
+                    currOperation = "";
+                    $($(".sidebar_button")[i]).css("background-color", "white");
+                    break;
+                }
+            }
+            currOperation = this.innerHTML;
+            $(this).css("background-color", "rgb(211, 138, 138)");
         }
     });
 });
